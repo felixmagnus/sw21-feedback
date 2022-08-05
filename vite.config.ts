4ebcc7e396678f80a8bc19ff618fa0709877fd96
Felix Magnus <felix.magnus@codecentric.de>
2022-08-05 15:12:47 +0200

    base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "sw21-feedback",
   build: {
     outDir: "docs",
   },
